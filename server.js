00bb38c76c3f06db9806b0f7799a4a2a4a9323bb
mdibrahim31 <mdibrahim.n.2018@gmail.com>
2026-09-14 15:45:52 +0600

    Add server listening log message

--- server.js
+++ server.js
@@ -56,3 +56,7 @@ app.get('/get-menu', async (req, res) => {
         res.status(500).json({ success: false, error: err.message });
     }
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
